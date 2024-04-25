813c04a4aac2027eeed95d85017e5edf1ea1ef87
Kevin Reynolds <kevin@k11s.io>
2024-04-25 17:30:17 -0400

    domain and validation

--- web-app/app/app.py
+++ web-app/app/app.py
@@ -8,6 +8,11 @@ import re
 
 app = Flask(__name__)
 
+def is_valid_domain(domain):
+    """Simple regex to validate a domain name."""
+    pattern = r'^[a-zA-Z\d-]{,63}(\.[a-zA-Z\d-]{,63})*$'
+    return re.match(pattern, domain) is not None
+
 @app.route('/')
 def index():
     with open("markdown/overview.md", "r") as file:
@@ -20,9 +25,16 @@ def setup():
     if request.method == 'POST':
         action = request.form['action']
         if action == 'save':
-            base_url = request.form['base_url']
+            base_url = request.form['base_url'].strip()
+            if not is_valid_domain(base_url):
+                # Handle invalid domain name
+                return render_template('setup.html', error="Invalid domain format.")
+            
+            # Assuming the domain is valid, you might want to prepend http:// or https://
+            base_url = 'http://' + base_url
+
             response = make_response(redirect('/setup'))
-            response.set_cookie('base_url', base_url, max_age=60*60*24)  # Set cookie for 1 year
+            response.set_cookie('base_url', base_url, max_age=60*60*24*365)  # Set cookie for 1 year
             return response
         elif action == 'clear':
             response = make_response(redirect('/setup'))
